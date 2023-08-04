a3754bad329f02982e16ec505d8545e852aebdbc
skyne98 <ahalahan@gmail.com>
2023-08-04 23:59:29 +0200

    feat: :sparkles: getting environment variables, calling OpenAI API

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,15 +1,24 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+use dotenv::dotenv;
 
-// Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
+// Command to read an environment variable
 #[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
+fn read_env_var(name: String) -> String {
+    dotenv().ok();
+    dotenv::var(name).unwrap_or_else(|_| "".to_string())
 }
 
 fn main() {
+    // Read and display the present environment variables
+    dotenv().ok();
+    println!("Environment variables:");
+    for (key, value) in dotenv::vars() {
+        println!("{}: {}", key, value);
+    }
+
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![greet])
+        .invoke_handler(tauri::generate_handler![read_env_var])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,28 @@
 import { createApp } from "vue";
 import "./styles.css";
 import App from "./App.vue";
+import { invoke } from '@tauri-apps/api/tauri';
+import { Configuration, OpenAIApi } from "openai";
+
+export async function read_env_var(name: string): Promise<string> {
+    const result = await invoke("read_env_var", { name: name });
+    if (typeof result === "string") {
+        return result;
+    }
+    throw new Error("Wrong type returned from read_env_var" + typeof result);
+}
+
+const openai_api_key = await read_env_var("OPENAI");
+console.log("openai_api_key: " + openai_api_key);
+const configuration = new Configuration({ apiKey: openai_api_key });
+delete configuration.baseOptions.headers['User-Agent'];
+
+// Make an embedding
+const openai = new OpenAIApi(configuration);
+const embedding = await openai.createEmbedding({
+    model: "text-embedding-ada-002",
+    input: ["Hello, my name is"],
+});
+console.log("embedding: " + JSON.stringify(embedding));
 
 createApp(App).mount("#app");
